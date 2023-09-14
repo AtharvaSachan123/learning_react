173fd52371eeb1e3a2fcd15b198fe967dc6b0fe8
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-09-14 22:35:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+const groom="salmon"
+const bride="Kat"
+let marks=90
+function changemark() {
+  marks=50;
+}
+  return (
+   <>
+   <h1 className=' font-bold text-5xl'>hello ji hello {marks}</h1>
+   <button className=' bg-slate-500 px-2 py-2 rounded text-white font-bold'>ydghja</button>
+   
+   </>
+  )
+}
+
+export default App
+
