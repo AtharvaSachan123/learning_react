3fe7daa482410acd49eb188eb709e54ca1ed2251
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-09-15 09:35:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+
+import Header from './components/Header';
 
 const App = () => {
-const groom="salmon"
-const bride="Kat"
-let marks=90
-function changemark() {
-  marks=50;
-}
+  const[user,setUser]= useState("Atharva");
   return (
-   <>
-   <h1 className=' font-bold text-5xl'>hello ji hello {marks}</h1>
-   <button className=' bg-slate-500 px-2 py-2 rounded text-white font-bold'>ydghja</button>
-   
-   </>
+    <div>
+     <Header user={user} />
+     {user}
+
+      <p className="tt">dildo</p>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Header = (props) => {
+
+
+  return (
+    <div>
+      <p>{props.user}</p>
+    </div>
+  )
+}
+
+export default Header
